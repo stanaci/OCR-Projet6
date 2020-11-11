fd47afe13709ef2a04339a21e3e66a6228690336
stanaci <59994967+stanaci@users.noreply.github.com>
2020-11-11 14:02:39 +0100

    Correction bouncer

--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,9 @@ const router = express.Router();
 
 const userCtrl = require('../controllers/user');
 // Creates a new instance of our bouncer (args optional)
-const bouncer = require ("express-bouncer")(2000, 900000);
+const bouncer = require ("express-bouncer")(500, 10000);
 
-router.post('/signup', bouncer.block, userCtrl.signup);
+router.post('/signup', userCtrl.signup);
 router.post('/login', bouncer.block, userCtrl.login);
 
 module.exports = router;
